75c4cda2dcb55cd9217e159d2466dc099531eef7
apeek <apeek@umd.edu>
2023-02-21 22:31:00 -0500

    Add editor page  Google API

---
+++ public/js/database.js
@@ -0,0 +1,11 @@
+
+require("dotenv").config({ path: path.resolve(__dirname, 'credentialsDontPost/.env') }) 
+
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { rawListeners } = require("process");
+const uri = `mongodb+srv://${userName}:${password}@cluster0.tyfa3jx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
