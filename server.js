e69e4dcd7069768be1aba84c435ca531efd583c5
Annemarie Peek <71455115+annemariepeek@users.noreply.github.com>
2024-10-07 23:24:38 -0400

    Some changes

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const fileupload = require('express-fileupload')
 process.stdin.setEncoding("utf8")
 let initial_path = path.join(__dirname, "src")
 
-// db config
-// require("dotenv").config({ path: path.resolve(__dirname, 'credentialsDontPost/.env') }) 
-// const userName = process.env.MONGO_DB_USERNAME
-// const password = process.env.MONGO_DB_PASSWORD
-// // const db = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION}
-// const { MongoClient } = require('mongodb')
-// const uri = `mongodb+srv://${userName}:${password}@cluster0.tyfa3jx.mongodb.net/?retryWrites=true&w=majority`
-// const client = new MongoClient(uri)
-
 const { db , client } = require('./db')
 const console = require("console")
 const { resourceLimits } = require("worker_threads")
